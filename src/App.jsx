76d57aa9affeee71005b5d13c760d3165d60548b
Dave <d.ruocco1987@googlemail.com>
2023-02-09 12:08:20 +0000

    Sets up API request and result sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Center, Image, Box } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 const App = () => {
   //state variables
-  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [randomIndex, setRandomIndex] = useState(0);
+  const [userScore, setUserScore] = useState(0);
 
   //make a request to the api and assign it to 'countries'
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then(({ data }) => {
-        setCountries(data);
+        const countryData = data;
+        const randomNumber = Math.random() * countryData.length;
+        const rounded = Math.floor(randomNumber);
+        setRandomIndex(rounded);
+        return countryData;
+      })
+      .then((countryData) => {
+        setCountry(countryData[randomIndex]);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, [setRandomIndex]);
 
   useEffect(() => {
-    console.log(countries[0]);
-  }, []);
+    console.log(country);
+    console.log(randomIndex);
+  }, [setRandomIndex]);
 
-  return <Heading>Guess the Flag!</Heading>;
+  return (
+    <Center>
+      <Heading>Guess the Flag!</Heading>
+    </Center>
+  );
 };
 
 export default App;
